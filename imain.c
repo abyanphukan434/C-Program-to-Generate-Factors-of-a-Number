7c2cb45586066120ebb877a7472a872f119dbaca
abyanphukan434 <72485127+abyanphukan434@users.noreply.github.com>
2020-12-19 11:56:10 +0530

    Create imain.c

---
+++ imain.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int n, factor = 1, i;
+  printf("Enter the number:");
+  scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+    {
+        factor = factor * i;
+    }
+    
+    printf("Factorial of the given number is = %d",factor);
+    return 0;
+}
